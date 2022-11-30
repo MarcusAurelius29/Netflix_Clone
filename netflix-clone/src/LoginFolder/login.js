38baf302181872fb1487df780d4657f29b694a17
MarcusAurelius29 <varungole1999@gmail.com>
2022-11-30 23:16:10 +0530

    yes

---
+++ netflix-clone/src/LoginFolder/login.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import './loginstyle.css';
+
+function login() {
+    return (
+ <div class="whole-page"> 
+
+ <div class = "title-header">
+    <h1> Login</h1>
+    </div>
+
+    <div class="background-image">
+
+    </div>
+
+ 
+ </div>
+
+ 
+
+    );
+}
+
+export default login;
