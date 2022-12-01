b4e814b4e8dfc5a1b4c170947009f83e1274d261
MarcusAurelius29 <varungole1999@gmail.com>
2022-12-01 14:05:58 +0530

    yes Created a new form with action button as submit

--- netflix-clone/src/LoginFolder/login.js
+++ netflix-clone/src/LoginFolder/login.js
@@ -1,19 +1,32 @@
 import React from 'react'
 import './loginstyle.css';
 
+
+
 function login() {
+
     return (
+
  <div class="whole-page"> 
 
  <div class = "title-header">
     <h1> Login</h1>
     </div>
 
-    <div class="background-image">
+    <div class="login-form">
+<form>
+    <label for="UserName">UserName</label><br></br>
+    <input type="text"></input><br></br>
+    <label for="password">Password</label><br></br>
+    <input type="password"></input><br></br>
+    <button>Submit</button>
 
+</form>
     </div>
+   
 
- 
+
+    
  </div>
 
  
